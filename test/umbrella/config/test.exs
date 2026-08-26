686e9a93291ffeee002a6ff0a17e844592074389
Danila Poyarkov <dev@dannote.net>
2026-08-26 14:18:09 +0200

    Expose umbrella browser test errors

--- test/umbrella/config/test.exs
+++ test/umbrella/config/test.exs
@@ -1,6 +1,7 @@
 import Config
 
 config :app_1, App1.Endpoint,
+  debug_errors: true,
   http: [ip: {127, 0, 0, 1}, port: 4002],
   secret_key_base: "cGXbx/UMm3qsyR8HYYVwO+/SWp1HAYxhFYHO/ugY6VaRenmQs4dCUoSzdxfMEMeS",
   server: true

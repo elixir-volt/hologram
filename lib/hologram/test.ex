3baffa0199463eea5a69a7a4d9094b5473d618bd
Danila Poyarkov <dev@dannote.net>
2026-08-26 17:31:46 +0200

    Wait for Hologram shutdown in browser setup

--- lib/hologram/test.ex
+++ lib/hologram/test.ex
@@ -16,7 +16,21 @@ defmodule Hologram.Test do
     # credo:disable-for-next-line Credo.Check.Design.AliasUsage
     Mix.Tasks.Compile.Hologram.run(force?: true)
 
+    supervisor = Process.whereis(Hologram.Supervisor)
+    monitor_ref = if supervisor, do: Process.monitor(supervisor)
+
     Application.stop(:hologram)
+    await_supervisor_shutdown(supervisor, monitor_ref)
     Application.ensure_all_started(:hologram)
+  end
+
+  defp await_supervisor_shutdown(nil, nil), do: :ok
+
+  defp await_supervisor_shutdown(supervisor, monitor_ref) do
+    receive do
+      {:DOWN, ^monitor_ref, :process, ^supervisor, _reason} -> :ok
+    after
+      5_000 -> raise "Hologram supervisor did not stop within 5 seconds"
+    end
   end
 end
